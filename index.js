8a4c411c20b5809a0f3c3daae44888759370e64a
frankda <suyunyi@gmail.com>
2024-07-03 15:23:20 +1000

    test puppeteer in render.com

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import * as cheerio from 'cheerio';
+import express from 'express';
 import { fetchHtmlData } from "./utils/requesets.js";
+import { scrapeWebsite } from "./puppeteer.js";
+
+const app = express();
+const port = 3000;
+app.get('/', async (req, res) => {
+    // res.send('hello');
+    const title = await scrapeWebsite();
+    res.send(title);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
 async function main() {
     const rootHtml = await fetchHtmlData('https://www.vodafone.com.au/');
@@ -10,4 +24,4 @@ async function main() {
     });
 }
 
-main();
+// main();
